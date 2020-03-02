4d6d41f83827cdbe52594f0b716cc561172c1e9c
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-03-02 13:19:06 +0800

    update method load data, when list account number not null, will never re-create new arraylist

--- src/com/mitrais/atm/repository/AccountRepository.java
+++ src/com/mitrais/atm/repository/AccountRepository.java
@@ -7,19 +7,22 @@ import java.util.List;
 
 public class AccountRepository {
     private static AccountRepository INIT;
+    private List<Account> accountList;
 
-    public static AccountRepository getInit(){
-        if(INIT == null)
+    public static AccountRepository getInit() {
+        if (INIT == null)
             INIT = new AccountRepository();
 
         return INIT;
     }
 
-    public List<Account> loadData(){
-        List<Account> accountList = new ArrayList<>();
+    public List<Account> loadData() {
+        if (accountList == null) {
+            accountList = new ArrayList<>();
+            accountList.add(new Account("John Doe", "112233", "012108", 100));
+            accountList.add(new Account("Jane Doe", "112244", "932012", 30));
+        }
 
-        accountList.add(new Account("John Doe", "112233", "012108", 100));
-        accountList.add(new Account("Jane Doe", "112244", "932012", 30));
         return accountList;
     }
 }
