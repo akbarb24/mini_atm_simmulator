da2c03202045ff71315f317ae236b58ff0caa7e8
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-03-02 08:40:51 +0800

    update main() method with calling welcome screen

--- src/com/mitrais/atm/Main.java
+++ src/com/mitrais/atm/Main.java
@@ -1,7 +1,9 @@
 package com.mitrais.atm;
 
+import com.mitrais.atm.screen.WelcomeScreen;
+
 public class Main {
     public static void main(String[] args) {
-
+        WelcomeScreen.getInit().show();
     }
 }
