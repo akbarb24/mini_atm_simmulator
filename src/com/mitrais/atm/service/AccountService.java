d0a928476c35e34dc7d6cc52904c194833ef53a8
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-03-02 08:36:32 +0800

    add AccountService

---
+++ src/com/mitrais/atm/service/AccountService.java
@@ -0,0 +1,25 @@
+package com.mitrais.atm.service;
+
+import com.mitrais.atm.model.Account;
+import com.mitrais.atm.repository.AccountRepository;
+
+public class AccountService {
+    private static AccountService INIT;
+    private AccountRepository accountRepository = AccountRepository.getInit();
+
+    public static AccountService getInit(){
+        if(INIT == null) {
+            INIT = new AccountService();
+        }
+
+        return INIT;
+    }
+
+    public Account auth(String accountNumber, String pin) {
+        return accountRepository.loadData().stream()
+                .filter(account -> accountNumber.equals(account.getAccountNumber())
+                        && pin.equals(account.getPin()))
+                .findAny()
+                .orElse(null);
+    }
+}
