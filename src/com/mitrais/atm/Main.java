2d641bcc5f669ef09a205142daad2df755e6699d
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-03-17 16:28:00 +0800

    load data from file first

--- src/com/mitrais/atm/Main.java
+++ src/com/mitrais/atm/Main.java
@@ -1,9 +1,12 @@
 package com.mitrais.atm;
 
+import com.mitrais.atm.repository.AccountRepository;
 import com.mitrais.atm.screen.WelcomeScreen;
 
 public class Main {
     public static void main(String[] args) {
+        AccountRepository.getInit().loadDataFile(args[0]);
+
         WelcomeScreen.getInit().show();
     }
 }
