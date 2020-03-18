2d964bc67196c68002408401c965cb489ffbaa55
Enjang_AB241 <EnjangAkbar.Binaji@mitrais.com>
2020-03-18 15:02:34 +0800

    add validation on load file

--- src/com/mitrais/atm/Main.java
+++ src/com/mitrais/atm/Main.java
@@ -5,8 +5,16 @@ import com.mitrais.atm.screen.WelcomeScreen;
 
 public class Main {
     public static void main(String[] args) {
-        AccountRepository.getInit().loadDataFile(args[0]);
-
+        loadAccountData(args);
         WelcomeScreen.getInit().show();
+    }
+
+    private static void loadAccountData(String[] args) {
+        if(args.length < 1) {
+            System.out.println("📣 Please include with filePath of the CSV data source file.");
+            System.out.println("   Example: /blabla/blabla/data.csv");
+        } else {
+            AccountRepository.getInit().loadDataFile(args[0]);
+        }
     }
 }
